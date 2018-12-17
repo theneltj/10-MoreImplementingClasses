d1d68fb2081f577f8526b24f69fdb8a94c18ce3a
theneltj <theneltj@rose-hulman.edu>
2018-12-17 09:35:35 -0500

    DONE

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -3,8 +3,8 @@ A   Baby   class and functions that use/test it.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Tyler Thenell.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -113,3 +113,4 @@ def main():
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
 main()
+class baby
