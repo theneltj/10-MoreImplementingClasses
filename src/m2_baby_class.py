45ee2d1177674c8ac15fad1982affe61bec9760b
theneltj <theneltj@rose-hulman.edu>
2018-12-17 15:30:26 -0500

    DONE

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -42,7 +42,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  Step 2a:  Implement a class called   Baby   that has a constructor method
 #            (__init__) and two other methods, as described below.
@@ -107,10 +107,27 @@ def main():
 #          """ Brief description of what an object of the class 'is'. """
 #
 ###############################################################################
+class Baby(object):
+    def __init__(self, name):
+        self.name = name
+        self.fedlast = 0
+        print('Hello baby {}!'.format(name))
+    def feed_baby(self):
+        print('Thank you for feeding baby {}.'.format(self.name))
+        self.fedlast = 0
+    def hour_passes(self):
+        if self.fedlast == 0:
+            print('Baby {} is sleeping.'.format(self.name))
+        if self.fedlast == 1:
+            print('Baby {} is awake. Time for food.'.format(self.name))
+        if self.fedlast >= 2:
+            print('Baby {} is CRYING uncontrollably! Feed the Baby!'.format(self.name))
+        self.fedlast += 1
 
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
 main()
-class baby
+
+
